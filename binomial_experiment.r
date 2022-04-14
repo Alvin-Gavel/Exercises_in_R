b9494cc3cb332b15652134b2d3f5afc30e1b0f43
Alvin-Gavel <alvin.gavel@gmail.com>
2022-04-14 08:03:52 +0200

    Created functions
    Instead of just having this as a script, I created a number of functions that do the thing that we're interested in. I also removed the sequential aspect entirely, so that it does not gradually feed successes and failures into the fit. While that is an interesting exercise in itself, you can do that more pedagogically by showing a sequence of appropriately chosen plots from this thing.

--- binomial_experiment.r
+++ binomial_experiment.r
@@ -1,28 +1,29 @@
-# This is based on a demonstration in Chap. 2 of Statistical Rethinking
-# 
-# The script will try to save 10 plots in a folder named
-# binomial_experiment_plots, which contain the successively updated
-# estimates of the probability of success in a binomial experiment, as
-# more and more samples are drawn
+# This will fit a binomial distribution to a given number of trials and
+# successes, and plot the fit. The intent is to give an idea of how the
+# distribution successively narrows as more data is supplied. This is
+# based on a demonstration in Chap. 2 of Statistical Rethinking by 
+# McElreath
+
 
 n_steps <- 100
-p <- seq(from=0, to=1, length.out=n_steps)
-prior <- rep(1, n_steps)
+binom_p <- seq(from=0, to=1, length.out=n_steps)
 
-true_p <- 0.7
-trials <- 10
-
-successes <- rbinom(trials, 1, true_p)
-so_far <- 0
-
-for (i in 1:trials) {
-   so_far <- so_far + successes[i]
-   L <- dbinom(so_far, size=i, prob=p)
-
-   unnorm_posterior <- L * prior
-   posterior <- unnorm_posterior / sum(unnorm_posterior)
-
-   jpeg(file=paste0("binomial_experiment_plots/Plot_", i ,".jpg"))
-   plot(p, posterior, type="l", xlab="p", ylab="P(p|data)")
+# Fit a binomial distribution to the given number of trials and
+# successes and plot it, dropping the plot in a folder named
+# binomial_experiment_plots
+binomial_fit <- function(trials, successes) {
+   binom_L <- dbinom(successes, size = trials, prob = binom_p)
+   jpeg(file=paste0("binomial_experiment_plots/Binomial_fit_", trials, "_", successes, ".jpg"))
+   plot(binom_p, binom_L, type="l", xlab="p", ylab="P(p|data)", xlim = c(0,1))
    dev.off()
 }
+
+# Fit each possible number of successes given the number of trials
+binomial_fits_by_trials <- function(trials) {
+   for (i in 0:trials) {
+      binomial_fit(trials, i)
+   }
+}
+
+trials <- 10
+binomial_fits_by_trials(trials)
