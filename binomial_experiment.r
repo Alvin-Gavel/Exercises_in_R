598d2d018767ab8eb5b362f8574a1c9619ffc25a
Alvin-Gavel <alvin.gavel@gmail.com>
2022-05-20 13:36:43 +0200

    Added function for making gif of successive updating

--- binomial_experiment.r
+++ binomial_experiment.r
@@ -4,6 +4,7 @@
 # based on a demonstration in Chap. 2 of Statistical Rethinking by 
 # McElreath
 
+library(animation)
 
 n_steps <- 100
 binom_p <- seq(from=0, to=1, length.out=n_steps)
@@ -45,3 +46,17 @@ plot_all_pP_below_n <- function(n_max, folder_path) {
    }
 }
 
+# Make a gif containing the binomial fits as they are updated over n
+# binomial draws with a probability P of success
+gif_random_sequence <- function(n_max, P, folder_path) {
+   tosses <- runif(n_max)
+   successes <- tosses < P
+   saveGIF(
+   for (n in 0:n_max) {
+      k = sum(successes[0:n], na.rm = TRUE)
+      pPnk = binomial_fit(n, k)
+      plot(binom_p, pPnk, type="l", xlab="P", ylab=paste0("p(P|", n, ", ", k, ")"), xlim = c(0,1), lwd = 2)
+   },
+   movie.name = paste0(folder_path, "/Updating_n_", n_max, "_P_", P, ".gif")
+   )
+}
