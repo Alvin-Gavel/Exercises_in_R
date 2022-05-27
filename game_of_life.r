9247b809da8cae4b071bc8527dc44f01226c7383
Alvin-Gavel <alvin.gavel@gmail.com>
2022-05-27 15:17:51 +0200

    Multiple edits, see longer note
    Added two functions for visual representation of grid. One of them gives text output, the other creates a gif. Added function for creating still life patterns, although it only makes one as yet. (I intend to add a function that turns strings into GoL boards). Made code somewhat more readable.

--- game_of_life.r
+++ game_of_life.r
@@ -12,16 +12,62 @@
 # 3. If the cell was dead and has exactly three live neighbours, then it is
 #    now alive.
 
+# This makes a warning about overlapping names (I think), I'll try to figure
+# out a fix
 library(pracma)
+library(caTools)
 
-update <- function(alive) {
-   count_live_neighbours = circshift(alive, c(0,1)) + circshift(alive, c(0,-1)) + circshift(alive, c(1,0)) + circshift(alive, c(-1,0)) + circshift(alive, c(1,1)) + circshift(alive, c(1,-1)) + circshift(alive, c(-1,1)) + circshift(alive, c(-1,-1))
+
+### The central game mechanic
+
+tick <- function(alive) {
+   count_live_neighbours = circshift(alive, c( 0, 1)) +
+                           circshift(alive, c( 0,-1)) +
+                           circshift(alive, c( 1, 0)) +
+                           circshift(alive, c(-1, 0)) +
+                           circshift(alive, c( 1, 1)) +
+                           circshift(alive, c( 1,-1)) +
+                           circshift(alive, c(-1, 1)) +
+                           circshift(alive, c(-1,-1))
    updated_alive = (alive & (count_live_neighbours >= 2 & count_live_neighbours <= 3)) | ((!alive) & count_live_neighbours == 3)
    return(updated_alive)
 }
+
+### Visualisation functions
+
+print_board <- function(board) {
+   zero_one_board = matrix(as.integer(board), nrow=nrow(board), ncol=ncol(board))
+   write.table(zero_one_board, row.names=F, col.names=F)
+}
+
+# This plotting is based on that used in the Wikipedia article on R. In
+# the binomial_experiment module I use a different approach. I don't
+# know which is best.
+plot_n_generations <- function(alive, n, filepath) {
+   X <- array(0, c(nrow(alive), ncol(alive), n))
+   for (i in 1:n) {
+     alive <- tick(alive)
+     X[, , i] <- alive
+   }
+   write.gif(X, filepath, col=rainbow(2), delay = 50)
+}
+
+### Board creation functions
 
 make_random_board <- function(x, y, P) {
    alive_vector = runif(x * y) < P
    alive = matrix(alive_vector, nrow = x, ncol = y)
    return(alive)
 }
+
+make_still_life <- function(name) {
+   if (name == 'block') {
+      alive = matrix(as.logical(matrix(c(0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0 ,0, 0))), nrow=4, ncol=4)
+   } else {
+      print("Cannot recognise pattern")
+   }
+   return(alive)
+}
+
+# At some point I will have to make a function for reading text input
+# as a board
